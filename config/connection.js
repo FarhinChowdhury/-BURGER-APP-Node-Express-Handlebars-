cfa989b32605f3b7670f1942041660c154331cbf
Farhin Chowdhury <farhinchowdhury0@gmail.com>
2020-11-14 16:08:59 -0500

    changed connection.js

--- config/connection.js
+++ config/connection.js
@@ -1,36 +1,26 @@
 const mysql = require( 'mysql' );
 
-// use this wrapper to create promise around mysql
-class Database {
-    constructor( config ) {
-        this.connection = mysql.createConnection( config );
-    }
-    query( sql, args=[] ) {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.query( sql, args, ( err, rows ) => {
-                if ( err )
-                    return reject( err );
-                resolve( rows );
-            } );
-        } );
-    }
-    close() {
-        return new Promise( ( resolve, reject ) => {
-            this.connection.end( err => {
-                if ( err )
-                    return reject( err );
-                resolve();
-            } );
-        } );
-    }
-  }
-// at top INIT DB connection
-const db = new Database({
-    host: "localhost",
-    port: 3306,
-    user: "root",
-    password: "rootroot",
-    database: "burgers_db"
-  });
 
-module.exports = db;
+var connection;
+if(process.env.JAWSDB_URL){
+    connection= mysql.createConnection(procexx.env.JAWSDB_URL);
+}else{
+    connection = mysql.createConnection({
+        host: 'localhost',
+        user: 'root',
+        password: 'rootroot',
+        database: 'burgers_db'
+    })
+}
+
+
+// at top INIT DB connection
+// const db = new Database({
+//     host: "localhost",
+//     port: 3306,
+//     user: "root",
+//     password: "rootroot",
+//     database: "burgers_db"
+//   });
+connection.connect();
+module.exports = connection;
