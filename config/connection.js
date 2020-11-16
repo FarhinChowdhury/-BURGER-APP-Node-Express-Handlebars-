ee6c92d5f34e28b4a5521bce56bb62e1b1ed03c1
Farhin Chowdhury <farhinchowdhury0@gmail.com>
2020-11-16 15:28:33 -0500

    deploying again

--- config/connection.js
+++ config/connection.js
@@ -1,26 +1,40 @@
 const mysql = require( 'mysql' );
 
+class Database {
+    constructor( config ) {
+        this.connection = mysql.createConnection( config );
+    }
+    query( sql, args=[] ) {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.query( sql, args, ( err, rows ) => {
+                if ( err )
+                    return reject( err );
+                resolve( rows );
+            } );
+        } );
+    }
+    close() {
+        return new Promise( ( resolve, reject ) => {
+            this.connection.end( err => {
+                if ( err )
+                    return reject( err );
+                resolve();
+            } );
+        } );
+    }
+  }
 
-var connection;
-if(process.env.JAWSDB_URL){
-    connection= mysql.createConnection(process.env.JAWSDB_URL);
-}else{
-    connection = mysql.createConnection({
-        host: 'localhost',
-        user: 'root',
-        password: 'rootroot',
-        database: 'burgers_db'
-    })
+var db;
+if (process.env.JAWSDB_URL) {
+    db = new Database(process.env.JAWSDB_URL);
+} else {
+    db = new Database({
+        host: "localhost",
+        port: 3306,
+        user: "root",
+        password: "rootroot",
+        database: "burgers_db"
+      });
 }
 
-
-// at top INIT DB connection
-// const db = new Database({
-//     host: "localhost",
-//     port: 3306,
-//     user: "root",
-//     password: "rootroot",
-//     database: "burgers_db"
-//   });
-connection.connect();
-module.exports = connection;
+module.exports = db;
