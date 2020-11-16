42e84a41a876d716c80cbf162a2dfa29034b5db5
Farhin Chowdhury <farhinchowdhury0@gmail.com>
2020-11-16 15:11:18 -0500

    fixxing bug

--- models/burgers.js
+++ models/burgers.js
@@ -2,16 +2,26 @@ const orm = require( '../config/orm' )
 //shows all available burgers
 async function Available(){
     const response = await orm.allBurgers()
+    if(!response){
+        return []
+    }
     return response.filter( item=>item.is_devoured == false )
 }
 //shows all devoured burgers
 async function Devoured(){
     const response = await orm.allBurgers()
+    if(!response){
+        return []
+    }
+    
     return response.filter( item=>item.is_devoured == true )
 }
 //function helps add a new burger
 async function add( name ){
     const response = await orm.newBurger( name )
+    if(!response){
+        return []
+    }
        return response
 }
 // function updates burgers to devoured
